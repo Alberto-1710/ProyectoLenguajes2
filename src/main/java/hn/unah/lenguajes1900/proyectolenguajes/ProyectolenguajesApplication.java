1621714664949a133c4f1847e646762a993d4511
Alberto-1710 <ajromero1710@gmail.com>
2024-04-14 20:57:53 -0600

    Inicio Proyecto SpringBoot

---
+++ src/main/java/hn/unah/lenguajes1900/proyectolenguajes/ProyectolenguajesApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes1900.proyectolenguajes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectolenguajesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectolenguajesApplication.class, args);
+	}
+
+}
